3b4df8dcb576e89a267e3515338dea1d20a5d830
Pratik Patil <pratik.patil@morrisonsplc.co.uk>
2022-07-23 22:39:18 +0530

    first commit

---
+++ create-auto-tables-mysql-using-spring-boot-hibernate/src/main/java/com/example/createautotablesmysqlusingspringboothibernate/CreateAutoTablesMysqlUsingSpringBootHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.createautotablesmysqlusingspringboothibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateAutoTablesMysqlUsingSpringBootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateAutoTablesMysqlUsingSpringBootHibernateApplication.class, args);
+	}
+
+}
---
+++ create-auto-tables-mysql-using-spring-boot-hibernate/src/test/java/com/example/createautotablesmysqlusingspringboothibernate/CreateAutoTablesMysqlUsingSpringBootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createautotablesmysqlusingspringboothibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateAutoTablesMysqlUsingSpringBootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
